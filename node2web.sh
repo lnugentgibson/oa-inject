a0ff1cc91062d4a46eb5e9d992886202a5fc384d
L. Nugent Gibson <lloyd.nugent.gibson@gmail.com>
2023-03-28 23:42:35 +0000

    updated node2web

--- node2web.sh
+++ node2web.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 cat "proxy.js"      | sed -ne '/web-start/,/web-end/ p' > "$1/inject.js"
+cat "array.js"      | sed -ne '/web-start/,/web-end/ p' >> "$1/inject.js"
 cat "graph.js"      | sed -ne '/web-start/,/web-end/ p' >> "$1/inject.js"
 cat "value.js"      | sed -ne '/web-start/,/web-end/ p' >> "$1/inject.js"
 cat "function.js"   | sed -ne '/web-start/,/web-end/ p' >> "$1/inject.js"
 cat "inject.js"     | sed -ne '/web-start/,/web-end/ p' >> "$1/inject.js"
-
-cat "base.js"       | sed -ne '/web-start/,/web-end/ p' > "$1/base.js"
-
-cat "js.js"         | sed -ne '/web-start/,/web-end/ p' > "$1/js.js"
-
-cat "$1/inject.js"  >  "$1/oa-inject.js"
-cat "$1/base.js"    >> "$1/oa-inject.js"
-cat "$1/js.js"      >> "$1/oa-inject.js"
+cat "dot.js"        | sed -ne '/web-start/,/web-end/ p' >> "$1/inject.js"
